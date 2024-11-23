357caad6209df66c5300fb0a48f4b7a55aa606d7
ziyadkassar619 <ziyadkassar619@gmail.com>
2024-11-22 22:43:48 -0800

    retrieved data for 10k seconds

--- jsonDataExtractor.py
+++ jsonDataExtractor.py
@@ -6,18 +6,20 @@ class JSONDataExtractor():
 
     def extractHeroInventory(self,match):
 
-        if match is not None:
-            data = [
-                {
-                    "hero_id": player["hero_id"],
-                    "item_0": player["item_0"],
-                    "item_1": player["item_1"],
-                    "item_2": player["item_2"],
-                    "item_3": player["item_3"],
-                    "item_4": player["item_4"],
-                    "item_5": player["item_5"],
-                }
-                for player in match["players"]
-            ]
-            return pd.DataFrame(data)
-        return None
+        try:
+            if match is not None:
+                data = [
+                    {
+                        "hero_id": player["hero_id"],
+                        "item_0": player["item_0"],
+                        "item_1": player["item_1"],
+                        "item_2": player["item_2"],
+                        "item_3": player["item_3"],
+                        "item_4": player["item_4"],
+                        "item_5": player["item_5"],
+                    }
+                    for player in match["players"]
+                ]
+                return pd.DataFrame(data)
+        except Exception as e:
+            return None
