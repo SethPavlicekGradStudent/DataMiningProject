52e1e619568e83eb1f38b6d2b7937a2795644fdc
SethPavlicek <spavlicek1172@sdsu.edu>
2024-11-22 19:42:11 -0800

    added matchids,, data extractor should work

--- dataExtract.py
+++ dataExtract.py
@@ -1,5 +1,5 @@
 from OpenDotaAPI import OpenDotaAPI
-from DataPreProcess2 import DataPreprocessing
+from jsonDataExtractor import JSONDataExtractor
 import time
 import json 
 import pandas as pd
@@ -7,38 +7,37 @@ import os
 
 def main(sleep_time = 2):
     api = OpenDotaAPI(verbose= True)
-    data = DataPreprocessing()
-    # recent_matches = filter_matches(api.get_recent_pro_matches())
-    df = pd.read_csv('large_amounts_of_data.csv')
-    i = 0
+    data = JSONDataExtractor()
+    # df = pd.read_csv('veryLargeAmountsOfData.csv')
+    # i = 0
     match_df = pd.read_csv("2022_Match_IDs.csv")
     match_ids = match_df["match_id"].iloc[20000:21000]
     while True:
         
         for recent_match in match_ids:
             match_details = api.get_match_info(recent_match)
-
+            table = None
             if match_details is not None:
-                table = data.get_all_current_match_tables(match_details)
+                table = data.extractHeroInventory(match_details)
             if table is not None:
-                if not (table.isna().any().any()) and not(table.shape[1] < 44) and not (recent_match in df[['MatchID']].values):
-                    file_path = 'large_amounts_of_data.csv'
-                    if os.path.isfile(file_path):
-                        # Append the DataFrame to the existing CSV file without overwriting
-                        table.to_csv(file_path, index=False, mode='a', header=False)  # Set header=False to avoid writing column headers
-                        print(table)
-                    else:
-                        # If the file doesn't exist, create a new CSV file
-                        table.to_csv(file_path, index=False)  
+                # if not (table.isna().any().any()):
+                file_path = 'veryLargeAmountsOfData.csv'
+                if os.path.isfile(file_path):
+                    # Append the DataFrame to the existing CSV file without overwriting
+                    table.df.to_csv(file_path, mode='a', header=not pd.io.common.file_exists(file_path), index=False)
+                    print(table)
+                else:
+                    # If the file doesn't exist, create a new CSV file
+                    table.to_csv(file_path, index=False)  
                 
-def filter_matches(matches_list):
-    return list(filter(lambda m: _filter_function(m), matches_list))
+# def filter_matches(matches_list):
+#     return list(filter(lambda m: _filter_function(m), matches_list))
 
-def _filter_function(match):
-    if match['duration'] < 1000 or match['duration'] > 4200:
-        return False
-    else:
-        return True
+# def _filter_function(match):
+#     if match['duration'] < 1000 or match['duration'] > 4200:
+#         return False
+#     else:
+#         return True
 
 if __name__ == "__main__":
     main()
---
+++ jsonDataExtractor.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+class JSONDataExtractor():
+    def __init__(self):
+        self.valid_df = True
+
+    def extractHeroInventory(self,match):
+
+        if match is not None:
+            data = [
+                {
+                    "hero_id": player["hero_id"],
+                    "item_0": player["item_0"],
+                    "item_1": player["item_1"],
+                    "item_2": player["item_2"],
+                    "item_3": player["item_3"],
+                    "item_4": player["item_4"],
+                    "item_5": player["item_5"],
+                }
+                for player in match["players"]
+            ]
+            return pd.DataFrame(data)
+        return None
